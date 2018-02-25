11c3912a4f84d6ef8dffb16befbd28b80d774994
koishimasato <m.koishi@mugenup.com>
2018-02-25 17:24:19 +0900

    Add create post mutation

--- app/graphql/mutations/create_post_mutation.rb
+++ app/graphql/mutations/create_post_mutation.rb
@@ -1,12 +1,18 @@
 Mutations::CreatePostMutation = GraphQL::Relay::Mutation.define do
   name "CreatePostMutation"
-  # TODO: define return fields
-  # return_field :post, Types::PostType
+  return_field :post, Types::PostType
 
-  # TODO: define arguments
-  # input_field :name, !types.String
+  input_field :subject, !types.String
 
-  resolve ->(obj, args, ctx) {
-    # TODO: define resolve function
+  resolve ->(obj, inputs, ctx) {
+    begin
+      post = ctx[:current_user].posts.build
+      post.subject = inputs.subject
+      post.save
+    rescue => e
+      return GraphQL::ExecutionError.new(e.message)
+    end
+
+    { post: post }
   }
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :createPostMutation, Mutations::CreatePostMutation.field
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
+  field :createPost, field: Mutations::CreatePostMutation.field
 end
