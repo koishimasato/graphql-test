20218f13dbc9734108417c871b046f3aece52353
koishimasato <m.koishi@mugenup.com>
2018-02-25 17:29:23 +0900

    Add update post mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,4 +4,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :updatePostMutation, Mutations::UpdatePostMutation.field
   field :createPostMutation, Mutations::CreatePostMutation.field
   field :createPost, field: Mutations::CreatePostMutation.field
+  field :updatePost, field: Mutations::UpdatePostMutation.field
 end
