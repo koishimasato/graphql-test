8e11ffa3550c290bdc387e3cfc5ec5a494925653
koishimasato <m.koishi@mugenup.com>
2018-02-25 17:09:54 +0900

    Add seed for Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'kaiji', email: 'kaiji@gmail.com')
-User.create!(name: 'akagi', email: 'akagi@gmail.com')
+user1 = User.find_or_create_by!(name: 'kaiji', email: 'kaiji@gmail.com')
+user2 = User.find_or_create_by!(name: 'akagi', email: 'akagi@gmail.com')
+
+Post.find_or_create_by!(user_id: user1.id,  subject: 'とても正気の沙汰とは思えない')
+Post.find_or_create_by!(user_id: user2.id, subject: '倍プッシュだ')
 
