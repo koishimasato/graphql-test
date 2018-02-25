1034cf18c1b89de168ed8c0efc81718668a7620e
koishimasato <m.koishi@mugenup.com>
2018-02-25 17:10:46 +0900

    $ bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225075952) do
+ActiveRecord::Schema.define(version: 20180225080919) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "user_id"
     t.string "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema.define(version: 20180225075952) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
