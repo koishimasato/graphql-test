bb6b00131f7202f960834bd15cfc81b224edea7e
koishimasato <m.koishi@mugenup.com>
2018-02-25 17:24:32 +0900

    $ bin/rails g graphql:mutation UpdatePostMutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
+  field :updatePostMutation, Mutations::UpdatePostMutation.field
   field :createPostMutation, Mutations::CreatePostMutation.field
   field :createPost, field: Mutations::CreatePostMutation.field
 end
